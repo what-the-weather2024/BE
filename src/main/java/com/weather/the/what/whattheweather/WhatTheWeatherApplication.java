339b26ca5557186065fc90385b0c5d9d5e843dca
jeongyuneo <piq2255@gmail.com>
2024-08-06 00:01:52 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/weather/the/what/whattheweather/WhatTheWeatherApplication.java
@@ -0,0 +1,12 @@
+package com.weather.the.what.whattheweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTheWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatTheWeatherApplication.class, args);
+    }
+}
---
+++ src/test/java/com/weather/the/what/whattheweather/WhatTheWeatherApplicationTests.java
@@ -0,0 +1,12 @@
+package com.weather.the.what.whattheweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTheWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
